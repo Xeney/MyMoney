dcb457d837d7716e6528de22512c75e1d3341fc5
Xeney <jeneksero@gmail.com>
2024-11-09 03:09:51 +0400

    update design and structure

--- main.py
+++ main.py
@@ -6,5 +6,25 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/secure')
+def secure():
+    return render_template('secure.html')
+
+@app.route('/feedback')
+def feedback():
+    return render_template('feedback.html')
+
+@app.route('/help')
+def help():
+    return render_template('help.html')
+
+@app.route('/sign-in')
+def sign_in():
+    return render_template('sign-in.html')
+
+@app.route('/sign-up')
+def sign_up():
+    return render_template('sign-up.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
