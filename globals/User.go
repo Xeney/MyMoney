0e5dd24c0f542aa56252b5e96522bbd28cd4d081
Xeney <jeneksero@gmail.com>
2024-11-27 18:59:36 +0400

    clean

--- globals/User.go
+++
@@ -1,9 +0,0 @@
-package globals
-
-// Структура пользователя
-type User struct {
-	Id       int    `json:"id"`
-	Name     string `json:"name"`
-	Login    string `json:"login"`
-	Password string `json:"password"`
-}
--- start.bash
+++
@@ -1,2 +0,0 @@
-docker build -t my_money:v0.1 .
-docker run -d -p 5000:5000 my_money:v0.1
